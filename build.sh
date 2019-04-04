51508b9e89e9f62f26bd2fcd25e560c7c531f5ab
denza <besicdenis@gmail.com>
2019-04-05 00:18:17 +0200

    Init project

---
+++ build.sh
@@ -0,0 +1,14 @@
+printf "#######################        Building docker image      ######################## \n"
+
+printf "# Set minikube context \n"
+eval $(minikube docker-env)
+
+printf "# Building Spring-boot image \n"
+docker build  -f docker/Dockerfile.Spring-boot -t spring-boot:1.0 .
+printf "# Spring-boot image built \n\n"
+
+printf "# Building quarkus image \n"
+docker build -f docker/Dockerfile.quarkus -t quarkus:1.0 .
+printf "# Quarkus image built \n\n"
+
+printf "#######################        Docker image built      ######################## \n"
